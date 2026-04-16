b83111c203a831f53cc966eaba699360451936f5
g81433416-netizen <g81433416@gmail.com>
2026-04-16 14:42:56 +0300

    Update Utils.cs

--- Models/Utils.cs
+++ Models/Utils.cs
@@ -1,19 +1,27 @@
 using System;
 using System.Collections.Generic;
 
+// Статический класс с вспомогательными методами
 public static class Utils
 {
+    // Обобщённый метод для обмена значений двух переменных
     public static void Swap<T>(ref T a, ref T b)
     {
+        // Временная переменная для хранения значения
         T temp = a;
+        
+        // Меняем местами
         a = b;
         b = temp;
     }
 
+    // Метод для вывода коллекции (массив, список и т.д.)
     public static void PrintCollection<T>(IEnumerable<T> collection)
     {
+        // Перебираем все элементы
         foreach (var item in collection)
         {
+            // Выводим каждый элемент
             Console.WriteLine(item);
         }
     }
