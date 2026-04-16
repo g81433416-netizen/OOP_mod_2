b5ab6cef0454d0ee17111fae2ac78a3b50954902
g81433416-netizen <g81433416@gmail.com>
2026-04-16 14:43:49 +0300

    Update Comparer.cs

--- Models/Comparer.cs
+++ Models/Comparer.cs
@@ -1,10 +1,17 @@
+// Обобщённый класс с ограничением:
+// T должен уметь сравниваться (реализовывать IComparable)
 public static class Comparer<T> where T : System.IComparable<T>
 {
+    // Метод для нахождения максимума
     public static T Max(T a, T b)
     {
+         // CompareTo:
+        // > 0 → a больше b
+        // < 0 → a меньше b
         return a.CompareTo(b) > 0 ? a : b;
     }
-
+    
+    // Метод для нахождения минимума
     public static T Min(T a, T b)
     {
         return a.CompareTo(b) < 0 ? a : b;
