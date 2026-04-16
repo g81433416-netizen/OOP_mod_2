4542e99d313570f7698a8d5c1d500cfcda71d5d0
g81433416-netizen <g81433416@gmail.com>
2026-04-16 14:46:43 +0300

    Update Person.cs

--- Models/Person.cs
+++ Models/Person.cs
@@ -1,8 +1,12 @@
+// Простой класс "Человек"
 public class Person
 {
+    //имя
     public string Name { get; set; }
+    //возраст
     public int Age { get; set; }
 
+    // Переопределение метода ToString для красивого вывода
     public override string ToString()
     {
         return $"{Name}, {Age}";
