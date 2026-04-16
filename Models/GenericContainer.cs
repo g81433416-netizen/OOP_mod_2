f7f73d1bda1c34fef05accd72301dd3bb5dd8e85
g81433416-netizen <g81433416@gmail.com>
2026-04-16 14:41:25 +0300

    Update GenericContainer.cs

--- Models/GenericContainer.cs
+++ Models/GenericContainer.cs
@@ -1,12 +1,16 @@
+// Обобщённый класс (generic), который может хранить значение любого типа T
 public class GenericContainer<T>
 {
+    // Приватное поле для хранения значения
     private T value;
 
+    // Метод для установки значения
     public void SetValue(T val)
     {
         value = val;
     }
 
+    // Метод для получения значения
     public T GetValue()
     {
         return value;
